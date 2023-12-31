eabe5065388335431354f654c481015b32ba55d6
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-01-01 10:09:29 +1100

    Make outputs conditional on machine build.

--- AWS/Terraform/locals.tf
+++ AWS/Terraform/locals.tf
@@ -1,8 +1,3 @@
 locals {
-  fleet_url        = "https://${aws_instance.logger[0].public_ip}:8412"
-  splunk_url       = "https://${aws_instance.logger[0].public_ip}:8000"
-  ata_url          = "https://${aws_instance.wef[0].public_ip}"
-  guacamole_url    = "http://${aws_instance.logger[0].public_ip}:8080/guacamole"
-  velociraptor_url = "https://${aws_instance.logger[0].public_ip}:9999"
   ip_whitelist     = ["${var.my_ip}/32", "${var.runner_ip}/32"]
 }
--- AWS/Terraform/outputs.tf
+++ AWS/Terraform/outputs.tf
@@ -3,37 +3,41 @@ output "region" {
 }
 
 output "logger_public_ip" {
-  value = aws_instance.logger[0].public_ip
+  value = length(aws_instance.logger) > 0 ? aws_instance.logger[0].public_ip : null
 }
 
 output "logger_ssh_access" {
-  value = "ssh vagrant@${aws_instance.logger[0].public_ip} (password=vagrant)"
+  value = length(aws_instance.logger) > 0 ? "ssh vagrant@${aws_instance.logger[0].public_ip} (password=vagrant)" : null 
 }
 
 output "dc_public_ip" {
-  value = aws_instance.dc[0].public_ip
+  value = length(aws_instance.dc) > 0 ? aws_instance.dc[0].public_ip : null
 }
 
 output "wef_public_ip" {
-  value = aws_instance.wef[0].public_ip
+  value = length(aws_instance.wef) > 0 ? aws_instance.wef[0].public_ip : null
 }
 
 output "win10_public_ip" {
-  value = aws_instance.win10[0].public_ip
+  value = length(aws_instance.win10) > 0 ? aws_instance.win10[0].public_ip : null
 }
 
 output "fleet_url" {
-  value = local.fleet_url
+  value = length(aws_instance.logger) > 0 ? "https://${aws_instance.logger[0].public_ip}:8412" : null
 }
 
 output "splunk_url" {
-  value = local.splunk_url
+  value = length(aws_instance.logger) > 0 ? "https://${aws_instance.logger[0].public_ip}:8000" : null
 }
 
 output "guacamole_url" {
-  value = local.guacamole_url
+  value = length(aws_instance.logger) > 0 ? "http://${aws_instance.logger[0].public_ip}:8080/guacamole" : null
 }
 
 output "velociraptor_url" {
-  value = local.velociraptor_url
+  value = length(aws_instance.logger) > 0 ? "https://${aws_instance.logger[0].public_ip}:9999" : null
+}
+
+output "ata_url" {
+  value = length(aws_instance.wef) > 0 ? "https://${aws_instance.wef[0].public_ip}" : null
 }
